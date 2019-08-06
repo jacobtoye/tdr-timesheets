ad069b870ba94df744f837b093d58b0cbe0a1bd3
Jacob Toye <jacob.toye@gmail.com>
2019-08-06 21:27:08 +1200

    Remove Emotion

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 
-import styled from '@emotion/styled';
 
-const Heading = styled.h1`
-  color: white
 `;
 
 const Login : React.FC = (props: any) => {
   return (
     <div className="h-screen bg-pink-900">
-      <Heading>Hello y'all</Heading>
     </div>
   );
 };
