c488bef0e13645817b19d355fe91b4c068dcc20b
Jacob Toye <jacob.toye@gmail.com>
2019-08-25 12:06:59 +1200

    Move routes to constants

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -4,6 +4,25 @@ import { useUserContext } from '../../screens/Login/UserContext';
 import MainLayout from '../MainLayout';
 import { BonusScreen, LeaveScreen, LoginScreen, TimesheetScreen, TodosScreen } from '../../screens';
 
+export const ROUTE_BONUS = '/bonus';
+export const ROUTE_LEAVE = '/leave';
+export const ROUTE_LOGIN = '/login';
+export const ROUTE_TIMESHEET = '/timesheet';
+export const ROUTE_TODOS = '/todos';
+
+export interface RouteConfig {
+  path: string;
+  name: string;
+  component: React.FC;
+}
+
+export const AuthedRoutes: Array<RouteConfig> = [
+  { path: ROUTE_BONUS, name: 'Bonus', component: BonusScreen },
+  { path: ROUTE_LEAVE, name: 'Leave', component: LeaveScreen },
+  { path: ROUTE_TIMESHEET, name: 'Timesheet', component: TimesheetScreen },
+  { path: ROUTE_TODOS, name: 'Todos', component: TodosScreen },
+];
+
 const Router = () => {
   const { userState } = useUserContext();
 
@@ -12,17 +31,16 @@ const Router = () => {
       {userState.user ? (
         <MainLayout>
           <Switch>
-            <Route path="/bonus" component={BonusScreen} />
-            <Route path="/leave" component={LeaveScreen} />
-            <Route path="/timesheet" component={TimesheetScreen} />
-            <Route path="/todos" component={TodosScreen} />
-            <Route path="*" render={() => <Redirect to="/timesheet" />} />
+            {AuthedRoutes.map(({ path, component }: RouteConfig) => (
+              <Route key={path} exact path={path} component={component} />
+            ))}
+            <Route path="*" render={() => <Redirect to={ROUTE_TIMESHEET} />} />
           </Switch>
         </MainLayout>
       ) : (
         <Switch>
-          <Route path="/login" component={LoginScreen} />
-          <Route path="*" render={() => <Redirect to="/login" />} />
+          <Route path={ROUTE_TODOS} component={LoginScreen} />
+          <Route path="*" render={() => <Redirect to={ROUTE_TODOS} />} />
         </Switch>
       )}
     </BrowserRouter>
--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
+import { AuthedRoutes, RouteConfig } from '../Router';
 
 const TabBarContainer = styled('div')`
   background-color: #ffffff;
@@ -42,18 +43,11 @@ const Tab = styled(NavLink)<TabProps>`
 const TabBar: React.FC<{}> = () => {
   return (
     <TabBarContainer>
-      <Tab to="/timesheet" activeClassName={activeClassName}>
-        Timesheet
-      </Tab>
-      <Tab to="/leave" activeClassName={activeClassName}>
-        Leave
-      </Tab>
-      <Tab to="/bonus" activeClassName={activeClassName}>
-        Bonus
-      </Tab>
-      <Tab to="/todos" activeClassName={activeClassName}>
-        Todos
-      </Tab>
+      {AuthedRoutes.map(({ path, name }: RouteConfig) => (
+        <Tab key={path} to={path} activeClassName={activeClassName}>
+          {name}
+        </Tab>
+      ))}
     </TabBarContainer>
   );
 };
