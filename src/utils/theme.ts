dc4c772153bf503f0ac6e396be808a0be35d7936
Jacob Toye <jacob.toye@gmail.com>
2019-09-05 06:14:57 +1200

    Add theme config

---
+++ src/utils/theme.ts
@@ -0,0 +1,46 @@
+const grid = {
+  BASELINE: 7,
+  MARGIN: 13,
+  GUTTER: 11,
+  COLUMN: 19,
+};
+
+const text = {
+  normal: {
+    size: 16,
+  },
+};
+
+const palette = {
+  background: {
+    MAIN: '#fbf7f0',
+    LIGHT: '#faf5ed',
+    DARK: '#f5f1ea',
+  },
+  ERROR: '#cc685e',
+  GREY: '#f5f1ea',
+  primaryColors: {
+    AQUA: '#68B5B1',
+    BLUE: '#4C83C0',
+    GREEN: '#68B579',
+    LIGHT_BLUE: '#62C8D3',
+    ORANGE: '#E6A573',
+    PINK: '#DF729D',
+    PURPLE: '#848CC5',
+    RED: '#cc685e',
+    YELLOW: '#E5BF73',
+  },
+  text: {
+    PRIMARY: '#645f59',
+    SECONDARY: '#a19d96',
+    GREY: '#b2aea8',
+  },
+};
+
+const theme = {
+  grid,
+  palette,
+  text,
+};
+
+export default theme;
