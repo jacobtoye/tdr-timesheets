040dfadb01ec3874f10992c62126e7c93d9b4719
Jacob Toye <jacob.toye@gmail.com>
2019-09-05 06:20:04 +1200

    Remove old UI from MainLayout

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
-import { AppBar, BaseScreen, NavBar } from '../index';
+import { BaseScreen } from '../index';
 import { TimesheetProvider } from '../../screens/Timesheet/TimesheetContext';
-
-const ContentContainer = styled('div')`
-  position: relative;
-`;
 
 const MainLayout: React.FC<{}> = ({ children }) => {
   return (
     <TimesheetProvider>
-      <BaseScreen>
-        <AppBar />
-        <NavBar />
-        <ContentContainer>{children}</ContentContainer>
-      </BaseScreen>
+      <BaseScreen>{children}</BaseScreen>
     </TimesheetProvider>
   );
 };
